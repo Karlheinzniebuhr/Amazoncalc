3ad114d7ed8ba08f529b50e236b5efd5466e5ca3
karl <karlbooklover@gmail.com>
2018-08-15 11:17:33 -0400

    fixed no asin error handling

--- calculator.py
+++ calculator.py
@@ -44,6 +44,7 @@ class MainPage(webapp2.RequestHandler):
                     n += 1
                     avg += value['venta']
             cotizacion = int(avg/n)
+            print("\n\nCotizacion del API = %s\n\n" % (cotizacion))
         except:
             e = sys.exc_info()[0]
             print str(e) + ' Dolar API error'
@@ -74,9 +75,12 @@ class Result(webapp2.RequestHandler):
                 return 0
 
         def getasin(url):
-            asin_list = re.findall(r"((?<=(?:\/dp\/))(.[^\/|\?]*)|(?<=(?:\/product\/))(.[^\/|\?]*))", url)
-            asin_str = asin_list[0][0]
-            return asin_str
+            try:
+                asin_list = re.findall(r"((?<=(?:\/dp\/))(.[^\/|\?]*)|(?<=(?:\/product\/))(.[^\/|\?]*))", url)
+                asin_str = asin_list[0][0]
+                return asin_str
+            except:
+                return False
 
         def test_getasin():
             urls = ['https://www.amazon.com/Nintendo-Switch-Gray-Joy-Con/dp/B01LTHP2ZK?tag=kasbl023-20',
@@ -84,59 +88,72 @@ class Result(webapp2.RequestHandler):
                 'https://www.amazon.com/gp/product/B01J94T4R2/ref=s9_acsd_top_hd_bw_b1LPqmx_c_x_w?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-4&pf_rd_r=TJE17ZS2T15YFMC47VTW&pf_rd_t=101&pf_rd_p=e07c9f6e-1414-50f2-b24d-f0d822b6c30a&pf_rd_i=1232597011',
                 'https://www.amazon.com/dp/B0106N5OOW/ref=s9_acsd_bw_wf_a_ECRouter_cdl_1?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-2&pf_rd_r=T13C28P1QDJNHF6NNJF9&pf_rd_t=101&pf_rd_p=4e5cb4ef-6898-4500-8a10-f50187471cec&pf_rd_i=12691227011',
                 'https://www.amazon.com/dp/B00PDLRHFW/ref=s9_acsd_bw_wf_a_ECRouter_cdi_0?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-2&pf_rd_r=T13C28P1QDJNHF6NNJF9&pf_rd_t=101&pf_rd_p=4e5cb4ef-6898-4500-8a10-f50187471cec&pf_rd_i=12691227011',
-                'https://www.amazon.com/Practical-Programming-Strength-Training-Rippetoe/dp/0982522754/ref=pd_rhf_se_s_vtp_ses_clicks_0_1?_encoding=UTF8&pd_rd_i=0982522754&pd_rd_r=8XGKPQHW14RSKY1A7V3Q&pd_rd_w=M1wB3&pd_rd_wg=uVf72&pf_rd_i=desktop-rhf&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=4743125097998253099&pf_rd_r=8XGKPQHW14RSKY1A7V3Q&pf_rd_s=desktop-rhf&pf_rd_t=40701&psc=1&refRID=8XGKPQHW14RSKY1A7V3Q'
+                'https://www.amazon.com/Practical-Programming-Strength-Training-Rippetoe/dp/0982522754/ref=pd_rhf_se_s_vtp_ses_clicks_0_1?_encoding=UTF8&pd_rd_i=0982522754&pd_rd_r=8XGKPQHW14RSKY1A7V3Q&pd_rd_w=M1wB3&pd_rd_wg=uVf72&pf_rd_i=desktop-rhf&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=4743125097998253099&pf_rd_r=8XGKPQHW14RSKY1A7V3Q&pf_rd_s=desktop-rhf&pf_rd_t=40701&psc=1&refRID=8XGKPQHW14RSKY1A7V3Q',
+                'https://www.amazon.com/gp/product/B003ZK58SQ/ref=s9_acsd_ri_bw_c_x_4_w?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-8&pf_rd_r=RC30RJCRNJHMPQK8KEH5&pf_rd_r=RC30RJCRNJHMPQK8KEH5&pf_rd_t=101&pf_rd_p=b8c0a303-a08e-4b0b-bd49-040811fd7080&pf_rd_p=b8c0a303-a08e-4b0b-bd49-040811fd7080&pf_rd_i=283155'
             ]
             for url in urls:
                 getasin(url)
+
+        # test_getasin()
 
         url = self.request.GET['url']
         precio_envio = extract_number('precio envio',self.request.GET['precio_envio'])
         cotizacion = int(extract_number('cotizacion',self.request.GET['cotizacion']))
 
+        print("Precio_envio: %s, Cotizacion: %s" % (precio_envio, cotizacion))
+
         # Get item ID from URL
         itemasin = getasin(url)
 
-        # find product data with the Amazon api
-        print "searching ASIN: " + str(itemasin)
-        try:
-            product = amazon.lookup(ItemId=itemasin, ResponseGroup='Offers, ItemAttributes', Condition='All', MerchantId='All')
-            precio = extract_number('precio',product.price_and_currency[0])
-            peso = extract_number('peso',product.get_attribute('PackageDimensions.Weight'))
+        if(itemasin):
+            # find product data with the Amazon api
+            print "searching ASIN: " + str(itemasin)
+            try:
+                print("amazon api:")
+                product = amazon.lookup(ItemId=itemasin, ResponseGroup='Offers, ItemAttributes', Condition='All', MerchantId='All')
+                precio = extract_number('precio',product.price_and_currency[0])
+                peso = extract_number('peso',product.get_attribute('PackageDimensions.Weight'))
 
-            precio_gs = precio * cotizacion
-            peso_g = peso * 0.01 * 453.492
+                precio_gs = precio * cotizacion
+                peso_g = peso * 0.01 * 453.492
 
-            precio_envio_gs = (peso_g/1000) * precio_envio * cotizacion
-            precio_total = precio_gs + precio_envio_gs
-            precio_up = precio_total * 1.3
-            precio_up = int(round(precio_up,-3))
+                precio_envio_gs = (peso_g/1000) * precio_envio * cotizacion
+                precio_total = precio_gs + precio_envio_gs
+                precio_up = precio_total * 1.3
+                precio_up = int(round(precio_up,-3))
 
+                template_values = {
+                    'precio': precio,
+                    'url': url,
+                    'precio_producto': str('{0:,}'.format(precio)),
+                    'precio_gs': str('{0:,}'.format(int(precio_gs))),
+                    'peso_envio': str('{0:,}'.format(int(peso_g))),
+                    'cotizacion': cotizacion,
+                    'precio_envio': str('{0:,}'.format(int(precio_envio))),
+                    'precio_envio_gs': str('{0:,}'.format(int(precio_envio_gs))),
+                    'precio_total': str('{0:,}'.format(int(precio_total))),
+                    'precio_up': str('{0:,}'.format(int(precio_up))),
+                }
+
+                template = JINJA_ENVIRONMENT.get_template('index.html')
+                self.response.write(template.render(template_values))
+            except:
+                e = sys.exc_info()
+                e2 = sys.exc_info()[0]
+                print str(e)
+                template_values = {
+                    'error': e,
+                    'error_summary': e2,
+                }
+                template = JINJA_ENVIRONMENT.get_template('index.html')
+                self.response.write(template.render(template_values))
+        else:
             template_values = {
-                'precio': precio,
-                'url': url,
-                'precio_producto': str('{0:,}'.format(precio)),
-                'precio_gs': str('{0:,}'.format(int(precio_gs))),
-                'peso_envio': str('{0:,}'.format(int(peso_g))),
-                'cotizacion': cotizacion,
-                'precio_envio': str('{0:,}'.format(int(precio_envio))),
-                'precio_envio_gs': str('{0:,}'.format(int(precio_envio_gs))),
-                'precio_total': str('{0:,}'.format(int(precio_total))),
-                'precio_up': str('{0:,}'.format(int(precio_up))),
-            }
-
+                    'error': "No se encontro un numero ASIN en la URL",
+                    'error_summary': "Favor, ingrese una URL completa con el ASIN!",
+                }
             template = JINJA_ENVIRONMENT.get_template('index.html')
             self.response.write(template.render(template_values))
-        except:
-            e = sys.exc_info()
-            e2 = sys.exc_info()[0]
-            print str(e)
-            template_values = {
-                'error': e,
-                'error_summary': e2,
-            }
-            template = JINJA_ENVIRONMENT.get_template('index.html')
-            self.response.write(template.render(template_values))
-
 
 # [START app]
 app = webapp2.WSGIApplication([
